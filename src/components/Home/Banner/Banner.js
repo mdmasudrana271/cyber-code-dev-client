6211a5558a72e0d0f7441a27e3d2eaa234172bc4
md masud rana <mr7123619@gmail.com>
2023-01-03 01:29:23 +0600

    adjust banner height

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -3,7 +3,7 @@ import banner from "../../../assets/images/banner.png";
 
 const Banner = () => {
   return (
-    <section className="text-white  md:flex items-center justify-evenly mx-10">
+    <section className="text-white  md:flex items-center justify-between mx-10 lg:h-[100vh]">
       <div className="flex flex-col justify-center md:p-6 text-start ">
         <h3 className="text-3xl font-bold leading-none sm:text-3xl font-bold">
           Online <span className="text-orange-500">education</span> courses
@@ -11,12 +11,10 @@ const Banner = () => {
         <h2 className="text-5xl font-bold">Millions of people learning</h2>
         <p className="mt-5">
           There are so many things to love about coding, both from a technical
-          and philosophical perspective. You can sharpen your mind, rewire your
-          brain, provide people with great software... the options and impact
-          are nearly endless.
+          and philosophical perspective.
         </p>
       </div>
-      <img src={banner} className="" alt="" />
+      <img src={banner} className="md:w-96" alt="" />
     </section>
   );
 };
